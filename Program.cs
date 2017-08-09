97bb595d58d76379dd23d706f66931c66d351180
BusterStratton <30865012+BusterStratton@users.noreply.github.com>
2017-08-09 09:28:38 -0400

    ok names were backwards red british blue german swapped them
    not all of these targets are airfields to be found in Gameplay.gpAirports 
    only those noted as airfields are airports so I don't think they will be found in the game as stated the airports will, but that is all

--- Program.cs
+++ Program.cs
@@ -12,36 +12,36 @@ namespace targets
 
             // Step 1 - Build up some objectives
             var objectives = new List<AirfieldTarget>(){
-                new AirfieldTarget("Abbeville Fuel", Objective.AbbevilleFuel, Team.German, 0, 10000),
-                new AirfieldTarget("Arras Fuel", Objective.ArrasFuel, Team.German, 0, 10000),
-                new AirfieldTarget("Bapaume Railyard", Objective.BapaumeRailyard, Team.German, 0, 10000),
-                new AirfieldTarget("LeTouque Fuel", Objective.LeTouqueFuel, Team.German, 0, 10000),
-                new AirfieldTarget("Abbeville Airfield", Objective.AbbevilleAirfield, Team.German, 0, 10000),
-                new AirfieldTarget("Ligescourt Airfield", Objective.LigescourtAirfield, Team.German, 0, 10000),
-                new AirfieldTarget("Barley Airfield", Objective.BarleyAirfield, Team.German, 0, 10000),
-                new AirfieldTarget("Tramcourt Airfield", Objective.TramcourtAirfield, Team.German, 0, 10000),
-                new AirfieldTarget("Arras ST Airfield", Objective.ArrasSTAirfield, Team.German, 0, 10000),
-                new AirfieldTarget("Cremont Airfield", Objective.CremontAirfield, Team.German, 0, 10000),
-                new AirfieldTarget("Ywrench Airfield", Objective.YwrenchAirfield, Team.German, 0, 10000),
-                new AirfieldTarget("Poix Nord Airfield", Objective.PoixNordAirfield, Team.British, 0, 10000),
-                new AirfieldTarget("Dieppe Airfield", Objective.DieppeAirfield, Team.British, 0, 10000),
-                new AirfieldTarget("Grandvilliers Airfield", Objective.GrandvilliersAirfield, Team.British, 0, 10000),
-                new AirfieldTarget("Brambos Airfield", Objective.BrambosAirfield, Team.British, 0, 10000),
-                new AirfieldTarget("Rosiers Airfield", Objective.RosiersAirfield, Team.British, 0, 10000),
-                new AirfieldTarget("Montdidea Airfield", Objective.MontdideaAirfield, Team.British, 0, 10000),
-                new AirfieldTarget("Haute Airfield", Objective.HauteAirfield, Team.British, 0, 10000),
-                new AirfieldTarget("Lehavre Airfield", Objective.LehavreAirfield, Team.British, 0, 10000),
-                new AirfieldTarget("Rouen Airfield", Objective.RouenAirfield, Team.British, 0, 10000),
+                new AirfieldTarget("Abbeville Fuel", Objective.AbbevilleFuel, Team.British, 0, 10000),
+                new AirfieldTarget("Arras Fuel", Objective.ArrasFuel, Team.British, 0, 10000),
+                new AirfieldTarget("Bapaume Railyard", Objective.BapaumeRailyard, Team.British, 0, 10000),
+                new AirfieldTarget("LeTouque Fuel", Objective.LeTouqueFuel, Team.British, 0, 10000),
+                new AirfieldTarget("Abbeville Airfield", Objective.AbbevilleAirfield, Team.British, 0, 10000),
+                new AirfieldTarget("Ligescourt Airfield", Objective.LigescourtAirfield, Team.British, 0, 10000),
+                new AirfieldTarget("Barley Airfield", Objective.BarleyAirfield, Team.British, 0, 10000),
+                new AirfieldTarget("Tramcourt Airfield", Objective.TramcourtAirfield, Team.British, 0, 10000),
+                new AirfieldTarget("Arras ST Airfield", Objective.ArrasSTAirfield, Team.British, 0, 10000),
+                new AirfieldTarget("Cremont Airfield", Objective.CremontAirfield, Team.British, 0, 10000),
+                new AirfieldTarget("Ywrench Airfield", Objective.YwrenchAirfield, Team.British, 0, 10000),
+                new AirfieldTarget("Poix Nord Airfield", Objective.PoixNordAirfield, Team.German, 0, 10000),
+                new AirfieldTarget("Dieppe Airfield", Objective.DieppeAirfield, Team.German, 0, 10000),
+                new AirfieldTarget("Grandvilliers Airfield", Objective.GrandvilliersAirfield, Team.German, 0, 10000),
+                new AirfieldTarget("Brambos Airfield", Objective.BrambosAirfield, Team.German, 0, 10000),
+                new AirfieldTarget("Rosiers Airfield", Objective.RosiersAirfield, Team.German, 0, 10000),
+                new AirfieldTarget("Montdidea Airfield", Objective.MontdideaAirfield, Team.German, 0, 10000),
+                new AirfieldTarget("Haute Airfield", Objective.HauteAirfield, Team.German, 0, 10000),
+                new AirfieldTarget("Lehavre Airfield", Objective.LehavreAirfield, Team.German, 0, 10000),
+                new AirfieldTarget("Rouen Airfield", Objective.RouenAirfield, Team.German, 0, 10000),
                 new AirfieldTarget("Crepon Airfield", Objective.CreponAirfield, Team.British, 0, 10000),
-                new AirfieldTarget("LeHavre Fuel 0", Objective.LeHavreFuel0, Team.British, 0, 10000),
-                new AirfieldTarget("LeHavre Fuel 2", Objective.LeHavreFuel2, Team.British, 0, 10000),
-                new AirfieldTarget("LeHavre Fuel 3", Objective.LeHavreFuel3, Team.British, 0, 10000),
-                new AirfieldTarget("Rouen Fuel", Objective.RouenFuel, Team.British, 0, 10000)
+                new AirfieldTarget("LeHavre Fuel 0", Objective.LeHavreFuel0, Team.German, 0, 10000),
+                new AirfieldTarget("LeHavre Fuel 2", Objective.LeHavreFuel2, Team.German, 0, 10000),
+                new AirfieldTarget("LeHavre Fuel 3", Objective.LeHavreFuel3, Team.German, 0, 10000),
+                new AirfieldTarget("Rouen Fuel", Objective.RouenFuel, Team.German, 0, 10000)
             };
 
             // Step 2 - Create the teams
-            var blueTeam = new ObjectiveTeam(Team.British, objectives.Where(s=> s.Team == Team.British));
-            var redTeam = new ObjectiveTeam(Team.German, objectives.Where(s=> s.Team == Team.German));
+            var blueTeam = new ObjectiveTeam(Team.German, objectives.Where(s=> s.Team == Team.German));
+            var redTeam = new ObjectiveTeam(Team.British, objectives.Where(s=> s.Team == Team.British));
 
             
 
